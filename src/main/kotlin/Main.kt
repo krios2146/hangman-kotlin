ecf0d6c29c9c519398513efbce5feae03a4a8462
krios2146 <krios2146@gmail.com>
2024-01-27 19:25:25 +0700

    feat: add reading input & choose word & hub functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,43 @@
 package dev.krios2146
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    println()
+    println("Welcome to the hangman game!")
+    startHub()
 }
+
+fun startHub() {
+    println()
+    println("[P]lay   [R]ules    [E]xit") // maybe results & settings later
+
+    val input = readUserInput()
+
+    when (input.uppercase()) {
+        "P" -> println("Game is started")
+        "R" -> println("Rules")
+        "E" -> return
+        else -> {
+            println("There is no such option")
+            startHub()
+        }
+    }
+}
+
+fun readUserInput(): String {
+    while (true) {
+        val input = readln()
+
+        when {
+            input.isBlank() -> println("Please, enter some letter or digit")
+            input.length > 1 -> println("Please, enter exactly one letter or digit")
+            !isAlphaNumeric(input) -> println("Please, enter letter or digit, no other symbols are allowed")
+            else -> return input
+        }
+    }
+}
+
+fun chooseWord(): String = File("dictionary.txt").readLines().random()
+
+fun isAlphaNumeric(string: String): Boolean = string.lowercase() in "a".."z" || string.lowercase() in "0".."9"
